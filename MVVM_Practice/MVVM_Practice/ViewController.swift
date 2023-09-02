48a24b0ff56207b3ccb6ca5f62eacbc7fbcf42eb
youz2me <kynhun20@gachon.ac.kr>
2023-09-02 18:00:01 +0900

    Feat: MVVM 실습 프로젝트 파일 Init

---
+++ MVVM_Practice/MVVM_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Practice
+//
+//  Created by YOUJIM on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
