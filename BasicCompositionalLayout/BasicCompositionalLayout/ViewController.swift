2c86c2ce08724e94ef60cc678a14bb1322b8f8aa
youz2me <kynhun20@gachon.ac.kr>
2023-09-12 22:00:01 +0900

    📁 MVVM 실습 파일 생성

---
+++ BasicCompositionalLayout/BasicCompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCompositionalLayout
+//
+//  Created by YOUJIM on 2023/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
